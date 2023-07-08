60f7132f5e23ab322d6fd8d356761c19a9647706
caitlinpedraja <caitlinpedraja3@gmail.com>
2023-07-08 14:43:56 -0500

    finish about page

--- flag-lookup/src/components/Pages/About.js
+++ flag-lookup/src/components/Pages/About.js
@@ -19,12 +19,13 @@ export const About = () => {
           Easily search for and sort through flags of any country generated from
           REST API.{" "}
         </p>
-
+        <div className="image_box_container">
         {images.map((image, index) => (
         <div className="image_box" key={index}>
           <img src={image.src} alt={image.alt} className="image" />
         </div>
       ))}
+        </div>
       </div>
     </div>
   );
