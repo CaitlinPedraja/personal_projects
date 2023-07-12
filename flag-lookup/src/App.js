0a59948fe1f2c0bac91466fdbc4353fea2d6fa4a
caitlinpedraja <caitlinpedraja3@gmail.com>
2023-07-11 19:35:48 -0500

    updated mod

--- flag-lookup/src/App.js
+++ flag-lookup/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./components/Pages/home_page";
+
 import { About } from "./components/Pages/About";
 import { Contact } from "./components/Pages/contacts";
-
+import CountriesInfo from "./components/countriesInfo/CountriesInfo";
+import  AllCountries  from "./components/Countries/AllCountries"
 function App() {
   return (
     <>
@@ -13,7 +14,8 @@ function App() {
 
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<AllCountries />} />
+            <Route path="/country/:countryName" element={<CountriesInfo />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
